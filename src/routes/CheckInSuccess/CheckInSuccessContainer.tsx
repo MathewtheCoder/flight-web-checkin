3475bf1b1976ef5807dc1678594a35ea0d3faa05
MathewTheCoder <jmathew1201@live.com>
2021-04-09 11:55:37 +0530

    feat: Add button to return back to home route

--- src/routes/CheckInSuccess/CheckInSuccessContainer.tsx
+++ src/routes/CheckInSuccess/CheckInSuccessContainer.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import {CheckCircleFilled} from '@ant-design/icons'
+import {Button} from 'antd'
+import {CheckCircleFilled, RollbackOutlined} from '@ant-design/icons'
+import { useHistory } from 'react-router-dom'
+import { ROOT_URL } from 'constants/routes'
+
 const CheckInSuccessContainer = () => {
+    const history = useHistory()
+    const navigateToCheckIn = () => history.push(ROOT_URL)
     return (
         <main className="checkinsuccess">
             <div className="wrapper">
-                <CheckCircleFilled />
+                <CheckCircleFilled className="check"/>
                 <h1>You've been checked in!</h1>
                 <p>Enjoy your flight!</p>
+                <Button
+                    type="primary"
+                    htmlType="button"
+                    className="actionBtn"
+                    block
+                    size="large"
+                    onClick={navigateToCheckIn}
+                >
+                    CheckIn for another flight <RollbackOutlined />
+                </Button>
             </div>
         </main>
     )
